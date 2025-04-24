2f4f5e4a584520654501a9ac9c9bb0b21f58279d
Ambrea <ambreawilliams97@gmail.com>
2025-04-24 16:54:07 +0000

    Added a new file with a module string and imported matplotlib.

---
+++ Lab15_AWilliams_1.py
@@ -0,0 +1,7 @@
+""" 
+Author: Ambrea Williams
+Date: 04/24/2025
+Project: Lab 15 / Graphing Data
+Description: This program will take a particular math formula and plot it on a graph.
+"""
+import matplotlib.pyplot as plt

3b4b156339d2e1c459ea5dfc726cf28e425cfcb0
Ambrea <ambreawilliams97@gmail.com>
2025-04-25 18:26:24 +0000

    Added more changes to requitments by installing pandas.

--- Lab15_AWilliams_1.py
+++ Lab15_AWilliams_1.py
@@ -5,3 +5,16 @@ Project: Lab 15 / Graphing Data
 Description: This program will take a particular math formula and plot it on a graph.
 """
 import matplotlib.pyplot as plt
+#import numpy as np
+
+ind_var = range (1, 6) # Independent variable
+dep_var = [x * x for x in ind_var] # Dependent variable
+
+"""Plotting the data"""
+figure, graph = plt.subplots() # Create a figure and axis
+graph.plot(ind_var, dep_var, linewidth=3, color='blue') # Plot the data
+graph. set_title('')
+
+plt.show()
+
+

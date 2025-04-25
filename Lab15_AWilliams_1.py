8dd759c07956591e686668d90bcc456796a42840
Ambrea <ambreawilliams97@gmail.com>
2025-04-25 18:47:54 +0000

    added a graph title and fixed requirements

--- Lab15_AWilliams_1.py
+++ Lab15_AWilliams_1.py
@@ -1,19 +1,21 @@
 """ 
 Author: Ambrea Williams
 Date: 04/24/2025
-Project: Lab 15 / Graphing Data
+Project: Graphing Data
+Unit 14: Lab 15, Opt 1
 Description: This program will take a particular math formula and plot it on a graph.
 """
 import matplotlib.pyplot as plt
 #import numpy as np
 
+"""Our data"""
 ind_var = range (1, 6) # Independent variable
 dep_var = [x * x for x in ind_var] # Dependent variable
 
 """Plotting the data"""
 figure, graph = plt.subplots() # Create a figure and axis
 graph.plot(ind_var, dep_var, linewidth=3, color='blue') # Plot the data
-graph. set_title('')
+graph.set_title('Graph of y = x^2')
 
 plt.show()
 

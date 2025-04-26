da2c16c5e1e0626ec00668943bfe767b7ca44254
Ambrea27-tingz <ambreawilliams97@gmail.com>
2025-04-26 19:18:59 -0400

    Set the x and y values on the graph output to not be visible.

--- random_walk_visual.py
+++ random_walk_visual.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
 # Create a RandomWalk instance
-rw = RandomWalk(100000)
+rw = RandomWalk(10000)
 rw.fill_walk()
 
 # Plotting
@@ -22,7 +22,7 @@ graph.scatter(0, 0, s=50, color='green', edgecolors='none')
 graph.scatter(rw.x_values[-1], rw.y_values[-1], s=50, color='red', edgecolors='none')
 
 # Remove axes
-#graph.get_xaxis().set_visible(False)
-#graph.get_yaxis().set_visible(False)
+graph.get_xaxis().set_visible(False)
+graph.get_yaxis().set_visible(False)
 
 plt.show()
